47a55961841d55e06874e6afb2dffaf7f840a3f3
fahimahammed <fahimahammed.cse@gmail.com>
2021-11-25 01:23:06 +0600

    insert a node

--- LinkedList/3-inserting-byPosition-and-according-to-sorting.c
+++ LinkedList/3-inserting-byPosition-and-according-to-sorting.c
@@ -1,3 +1,5 @@
+/*3. Inserting: a) inset a node in nth position. b) insert a node in sorted list*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -9,16 +11,41 @@ struct node{
 void createList(int n);
 void printList();
 void insertAccSort(int n);
+void insertByPos(int value, int loc, struct node *head);
 
 int main()
 {
-    int n;
+    int n, select, value, pos;
     printf("Enter total number of nodes: ");
     scanf("%d", &n);
 
     createList(n);
     printList();
-    insertAccSort(n);
+
+    printf("\n MENU \n\t1. Insert by postion.\n\t2. Insert according to ascending oreder.\n\t0. Exit\n   Enter your choice: ");
+    scanf("%d", &select);
+    switch(select)
+    {
+        case 1:
+            printf("\n Enter the position: ");
+            scanf("%d", &pos);
+            printf("\n Enter the data: ");
+            scanf("%d", &value);
+            insertByPos(value, pos, head);
+            break;
+
+        case 2:
+            insertAccSort(n);
+            break;
+
+        case 0:
+            exit(0);
+            break;
+
+        default:
+            printf("\n Please enter valid input.\n");
+        }
+
     return 0;
 }
 
@@ -82,29 +109,43 @@ void insertAccSort(int n)
 
     temp = head;
     for(i=1; i<=n; i++){
-        //printf(" %d ", temp->data);
         tmp = temp->data;
         if(value > tmp){
             loc++;
-            //newNode = (struct node *)malloc(sizeof(struct node));
-            //newNode->data = value;
-            //newNode->next = temp->next;
-            //temp->next = newNode;
         }
         temp = temp->next;
-        printf("loc: %d\n", loc);
+
+    }
+    //printf("loc: %d\n", loc);
+    insertByPos(value, loc, head);
+}
+
+void insertByPos(int value, int loc, struct node *head1)
+{
+    struct node *temp, *newNode;
+    int i;
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    if(newNode == NULL){
+        printf("Unable to allocate memory.\n");
+        exit(0);
     }
 
-    for(i=1; i<=loc; i++){
-        if(i == loc){
-            newNode = (struct node *)malloc(sizeof(struct node));
-            newNode->data = value;
-            newNode->next = temp->next;
-            temp->next = newNode;
-        }
+    newNode->data = value;
+    newNode->next = NULL;
+
+    temp = head1;
+    for(i=2; i<loc; i++){
         temp = temp->next;
+        //temp->next = newNode;
+        if(temp == NULL){
+            break;
+        }
     }
-    printf("loc: %d\n", loc);
+    if(temp != NULL){
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
     printList();
 }
 
